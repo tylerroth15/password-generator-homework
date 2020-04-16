b19491bb79bb3b1430a264a9096ea1f45c811730
Tyler Roth <tylerroth1535@gmail.com>
2020-04-15 20:58:31 -0400

    updated work on JS

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,48 @@ function writePassword() {
 
 }
 
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+
+// var all = uppercase + lowercase + numbers + symbols;
+// var password = '';
+// for (var i=0; i< 128; i++){
+//     password += all.substring(character, character +1);
+// }
+// return password;
+
+// This is not the correct code but keeping for Event Listner Ideas
+// document.onload = function (){
+//     var generateButton = document.querySelector('#generate');
+//     generateButton.addEventListener('click', function(){				
+//         document.querySelector('#password').value = generate();
+//     });
+// }
+
 function generatePassword (){
-  var userInput = prompt("special character", "lower case", "how many characters")
-  return ""
+  var userInput = prompt("How long would you like your password? Must be 8-128 characters.","Character Types: Would you like to include lower case? Y/N", "Character Types: Would you like to include Uppercase? Y/N?", "Character Types: Would you like to include numeric characters? Y/N?", "Character Types: Would you like to include special characters? Y/N?")
+ 
+}
+if (userInput <8) 
+    alert ("Too Few Characters");
+else (userInput > 128)
+    alert ("Too Many Characters");
+
+if (userInput)
+
+
+// ideas for functions and for loops
+
+// function password(1,characters){
+// var pwd = 'string';
+
+// for (var i=0; i<l; i++){
+//     pwd += characters.charAt(Math.floor(Math.random()*characters.length));}
+//     return pwd;
 }
 
-if/else statements
+// submit.addEventListener("click", function())
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
